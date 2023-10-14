6567ee9de57f6c883390d8266ea42e9854d998c2
Rawitch Payakkawan <rawitch13@gmail.com>
2023-10-14 17:35:31 +0700

    fix cors

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 //middlewares
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
@@ -47,7 +47,7 @@ const myUploadMiddleware = upload.single('file');
 //   res.status(200).json(file.filename);
 // });
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
